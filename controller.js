197efe5a6b3225af5d55d0fa9f8f3d6a8df96b03
Rob DelValle <robdel13@gmail.com>
2016-11-16 13:33:25 -0700

    http functinality and paging added

--- controller.js
+++ controller.js
@@ -1,10 +1,28 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
 
+	$scope.currentPage = 1;
+
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+  	var promise = mainService.getUsers($scope.currentPage);
+  	promise.then(function(response) {
+  		$scope.users = response.data.data;
+  		$scope.totalPages = response.data.total_pages;
+  	})
   }
 
-
   $scope.getUsers();
+  $scope.prev = function() {
+  	if ($scope.currentPage > 1) {
+  		$scope.currentPage--;
+  		$scope.getUsers();
+  	}
+  	
+  };
 
+  $scope.next = function() {
+  	if($scope.currentPage < $scope.totalPages) {
+  		$scope.currentPage++;
+  		$scope.getUsers();
+  	}
+  }
 });
